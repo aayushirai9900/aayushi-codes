b9e6cd7c57101f4d1d276103b239fd271a6924f1
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-09-15 11:11:52 +0530

    Converted mp4 to mp3

--- code.py
+++ code.py
@@ -1,2 +1,4 @@
-import moviepy.editor
-videoclip=VideoFileClip("enter file name here:")
+from moviepy.editor import *
+videoclip = VideoFileClip('Enter mp4 file name here')
+audio = videoclip.audio
+audio.write_audiofile('Enter mp3 file name to be created here')
