bf7516134e60d73c73499ff36e69603da10f87c7
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-10-01 12:56:44 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,2 +1,4 @@
-import moviepy.editor
-videoclip=VideoFileClip("enter file name here:")
+from moviepy.editor import *                                         #pip install moviepy
+videoclip = VideoFileClip('Enter mp4 file name here')                #enter file name and path of the file which will be converted
+audio = videoclip.audio
+audio.write_audiofile('Enter mp3 file name to be created here')      #enter the file name and path where the file should be saved.
