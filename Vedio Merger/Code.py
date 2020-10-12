0816b8e61dd84276849607b0e19c16d82a48a983
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-10-12 16:20:48 +0530

    Update Code.py

--- Vedio Merger/Code.py
+++ Vedio Merger/Code.py
@@ -1,5 +1,6 @@
-import moviepy.editor
-video1 = moviepy.editor.VideoFileClip("Vedio Merger/1.mp4")
-video2 = moviepy.editor.VideoFileClip("Vedio Merger/2.mp4")
-finalvideo = moviepy.editor.concatenate_videoclips([video1,video2])
-finalvideo.write_videofile("Final.mp4",codec='libx264')
+import moviepy.editor                                                         #pip install moviepy
+video1 = moviepy.editor.VideoFileClip("Vedio Merger/1.mp4")                   # Tking clip to merge     #add .subclip(a,b) to jus merge subclip from the full clip
+video2 = moviepy.editor.VideoFileClip("Vedio Merger/2.mp4")                   # Tking clip to merge     #add .subclip(a,b) to jus merge subclip from the full clip
+finalvideo = moviepy.editor.concatenate_videoclips([video1,video2])           # merging video
+finalvideo.write_videofile("Final.mp4",codec='libx264')                       # encoding and writing video in desired name.
+ 
